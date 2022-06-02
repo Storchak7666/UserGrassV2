fadbb800d3eb8cd4d05f3b0a86d34e2b2e4b60b4
Oleg <oleg.svachiy.ninetynine19@gmail.com>
2022-06-02 17:50:17 +0300

    Add deseaseList

--- internal/domain/user/user.go
+++
@@ -1,9 +0,0 @@
-package user
-
-type User struct {
-	Id      int64  `db:"Id,omitempty"`
-	Name    string `db:"Name"`
-	Age     int64  `db:"Age"`
-	City    string `db:"City"`
-	Country string `db:"Country"`
-}
